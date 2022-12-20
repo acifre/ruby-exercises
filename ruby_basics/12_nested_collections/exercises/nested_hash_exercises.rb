7a03d093e42a7a5feb73a1a413d4217c4f22c12c
acifre <78503928+acifre@users.noreply.github.com>
2022-12-20 16:45:33 -0500

    Finish nested hashes exercises

--- ruby_basics/12_nested_collections/exercises/nested_hash_exercises.rb
+++ ruby_basics/12_nested_collections/exercises/nested_hash_exercises.rb
@@ -2,6 +2,8 @@ def find_language_information(languages, language_name)
   # Take languages (a nested hash) and language_name as a symbol, return the
   # value for the language_name key (which will be another hash!)
 
+  languages.fetch(language_name)
+
   # the languages hash will look something like this:
   # {
   #   ruby: { initial_release: 'December 25, 1996', is_beautiful?: true },
@@ -11,27 +13,47 @@ end
 
 def add_information_about_language(languages, language_name, info_key, info_value)
   # Take languages and add the key/value pair info_key/info_value to the nested
+
+  languages[language_name][info_key] = info_value
   # hash of language_name, then return the updated languages hash
+
+  languages
+
+
 end
 
 def add_language(languages, language_name, language_info_value)
   # Take languages and add the key/value pair language_name/language_info_value
+  languages[language_name] = language_info_value
   # to it, then return languages
+  languages
 end
 
 def delete_information_about_language(languages, language_name, info_key)
   # Take languages and delete the key/value pair with key info_key from
+
+  languages[language_name].delete(info_key)
   # language_name, then return languages
+  languages
 end
 
 def delete_language(languages, language_name)
   # Take languages and delete the language_name key/value pair, then return
+
+  languages.delete(language_name)
   # languages
+
+  languages
 end
 
 def find_beautiful_languages(languages)
   # Take languages and return a hash containing only languages which have the
+
+  languages.select { |name, hash| name[hash][:is_beautiful?] == true }
+
   # key/value pair { is_beautiful?: true } listed in their information
+
+
 end
 
 def find_language_facts(languages, language_name, fact_index = 0)
